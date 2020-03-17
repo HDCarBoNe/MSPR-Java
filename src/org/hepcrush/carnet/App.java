14c394e70352089e59f98fc1cd893f3fdfa06c54
Dylan <dylan.gogeta@gmail.com>
2020-03-17 13:28:30 +0100

    LoginFrame
    LoginFrame OK

--- src/org/hepcrush/carnet/App.java
+++ src/org/hepcrush/carnet/App.java
@@ -1,7 +1,20 @@
 package org.hepcrush.carnet;
 
-public class App {
-    public static void main(String[] args) {
+import org.hepcrush.carnet.bo.User;
+import org.hepcrush.carnet.dal.UserDAO;
+import org.hepcrush.carnet.swing.*;
 
+import javax.swing.*;
+import java.sql.SQLException;
+
+public class App {
+
+    public static void main(String[] args) {
+        LoginFrame frame = new LoginFrame();
+        frame.setTitle("Login Form");
+        frame.setVisible(true);
+        frame.setBounds(10, 10, 370, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
     }
 }
